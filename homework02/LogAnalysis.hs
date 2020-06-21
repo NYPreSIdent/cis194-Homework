9c2b4e6b577792be515970aab1e9422925e795b1
Zhiyang Wang <liasosong546263@gmail.com>
2020-06-21 17:36:13 +0800

    homework2 completed.

--- homework02/LogAnalysis.hs
+++ homework02/LogAnalysis.hs
@@ -17,11 +17,11 @@ parse messages  = map parseMessage (lines messages)
 
 -- Exercise 2
 insert :: LogMessage -> MessageTree -> MessageTree
+insert m Leaf                      = Node Leaf m Leaf
 insert (Unknown _) originTree      = originTree
-insert m@(LogMessage _ _ _) Leaf = Node Leaf m Leaf
-insert o@(LogMessage _ timestamp _) (Node leftH (LogMessage _ otimestamp _) rightH)
-    | timestamp >= otimestamp      = Node leftH o (insert o rightH)
-    | otherwise                    = Node (insert o leftH) o rightH
+insert o@(LogMessage _ timestamp _) (Node leftH ori@(LogMessage _ otimestamp _) rightH)
+    | timestamp >= otimestamp      = Node leftH ori (insert o rightH)
+    | otherwise                    = Node (insert o leftH) ori rightH
 insert _ tree                      = tree
 
 -- Exercise 3
@@ -35,12 +35,17 @@ inOrder Leaf             = []
 inOrder (Node leftH mes rightH) = (inOrder leftH) ++ [mes] ++ inOrder rightH
 
 -- Exercise 5
-whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong logmes  = case inOrder $ build logmes of
-    ((LogMessage (Error level) _ mes) : remain)
-        | level >= 50 -> mes : whatWentWrong remain
-        | otherwise   -> []  : whatWentWrong remain
-    ((LogMessage _ _ _) : remain) -> whatWentWrong remain
-    ((Unknown _) : remain)        -> whatWentWrong remain
-    []                -> []
+-- Solution depends on @jumblesale from his GitHub.
 
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong mess = transToString $ inOrder $ build (filter (isCondition 50) mess) 
+
+isCondition :: Int -> LogMessage -> Bool
+isCondition minLevel (LogMessage (Error level) _ _)
+    | level > minLevel = True
+    | otherwise        = False
+isCondition _ _        = False
+
+transToString :: [LogMessage] -> [String]
+transToString (LogMessage _ _ msg : msgs) = msg : transToString msgs
+transToString _                           = []
